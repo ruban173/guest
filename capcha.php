adb62d56767ffe884e992f80c677a5e0687363d2
alex <alex@bk.ru>
2018-03-17 11:58:46 +0300

    Captcha

--- capcha.php
+++ capcha.php
@@ -1,4 +1,87 @@
 <?php
+
+class Captcha{
+
+    private $text;
+    private $collection;
+    private $width;
+    private $height;
+    private $image;
+    private $object;
+
+    function __construct()
+    {
+        $this->object=$this;
+    }
+
+    private function geterate($count=5){
+      for ($i=0;$i< $count;$i++)
+          $this->text.= $this->collection[rand(0,count($this->collection))];
+      return $this;
+    }
+
+    public function upper(){
+        $array=range('A','Z');
+         $this->collection=(empty( $this->collection))?$array:array_merge_recursive($this->collection,$array);
+         return $this;
+    }
+    public function lower(){
+        $array=range('a','z');
+        $this->collection=(empty( $this->collection))?$array:array_merge_recursive($this->collection,$array);
+        return $this;
+    }
+    public function number(){
+        $array=range(0,9);
+        $this->collection=(empty( $this->collection))?$array:array_merge_recursive($this->collection,$array);
+        return $this;
+    }
+    public function getCollection(){
+        return  $this->collection;
+    }
+
+    public function create($width=120,$height=40){
+        $this->geterate();
+        header("Content-Type: image/png");
+        $this->image = imagecreate($width,$height);
+        imagecolorallocate($this->image, 160, 160, 160);
+        $color = imagecolorallocate($this->image, 150, 0, 91);
+        for ($i=0;$i<strlen($this->text);$i++){
+            $font=rand(4,5);
+            $x+=rand(15,25);
+            $y=rand(5,20);
+            imagechar($this->image, $font, $x, $y,  $this->text{$i}, $color);
+        };
+
+        $pixel_color = imagecolorallocate($this->image, 0,160,255);
+        for($i=0;$i<500;$i++) {
+            imagesetpixel($this->image,rand()%200,rand()%50,$pixel_color);
+        }
+
+
+        imagepng($this->image);
+        imagedestroy($this->image);
+    }
+
+}
+
+(new Captcha)->upper()
+                ->lower()
+                ->number()
+                ->create();
+
+/*
+$captcha=new Captcha();
+$captcha->upper()
+         ->lower()
+         ->number();
+$captcha->create();
+
+
+
+/*echo '<pre>';
+print_r( $captcha->geterate()) ;
+*/
+/*
 function generateStr( $count=1){
 
     $array_a = range('a','z');
@@ -41,6 +124,6 @@ imagedestroy($im);
 
 
  //print_r(generateStr(1));
-
+*/
 
 ?>
